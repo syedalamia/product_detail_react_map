1e91d0ffb6a2fca2522cae3be0bfd760d44f51e0
lamianoorjahan <62371017+lamianoorjahan@users.noreply.github.com>
2021-02-27 23:50:50 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ProductDetail from './ProductDetail'
+
+function App() {
+  return (
+    <div className="App">
+      <ProductDetail/>
+    </div>
+  );
+}
+
+export default App;
